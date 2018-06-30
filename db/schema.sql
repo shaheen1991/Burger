544a30cac35be40a4668dbb7a04b2f335f81104e
shaheen1991 <shaheen_11991@hotmail.com>
2018-06-30 14:14:07 -0500

    commit

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers
+(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT FALSE,
+  date TIMESTAMP
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Double');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
